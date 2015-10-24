0411d870e55026fea1d91d35bfd0575dcbbb4a8d
Q-Max <a1u1usul3@gmail.com>
2015-10-24 17:05:44 +0800

    MPI_IO

--- basic_oesort.c
+++ basic_oesort.c
@@ -78,12 +78,12 @@ int main (int argc, char *argv[]) {
 		return 0;
 	}
 
-	int N = atoi(argv[1]), alloc_num;
+	int N = atoi(argv[1]), alloc_num, former_alloc_num=0, last_alloc_num=0;
 	const char *inName = argv[2];
 	const char *outName = argv[3];
 	double start, finish;
-	int *root_arr; // for root process (which rank == 0) only
-
+	int *root_ptr; // for root process (which rank == 0) only
+  
 	// Part 1: Read file
 	/* Note: You should deal with cases where (N < size) in Homework 1 */
 	int rc;
@@ -98,6 +98,7 @@ int main (int argc, char *argv[]) {
 		puts("N is bigger than testcase in input file, read to the end");
 		N = total_number_of_bytes/sizeof(int);
 	}
+
 	// sheu if N < # of processes?
 	int *array;
 	alloc_num = N/size;
@@ -143,10 +144,12 @@ int main (int argc, char *argv[]) {
 	}
 	else if((alloc_num)%2){
 		alloc_num--;
-		MPI_File_seek(fp,(MPI_Offset)alloc_num*rank, MPI_SEEK_SET);
+    former_alloc_num = alloc_num;
+		MPI_File_seek(fp,(MPI_Offset)alloc_num*rank*sizeof(int), MPI_SEEK_SET);
 		if(rank==size-1){
 			alloc_num = N - alloc_num * rank;
 		}
+    last_alloc_num = N - alloc_num * rank;
 		array = (int*)malloc(sizeof(int)*alloc_num);
 		start = MPI_Wtime();
 		MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
@@ -155,24 +158,38 @@ int main (int argc, char *argv[]) {
 	}
 	else{
 		// last process need deal with more inputs
+    former_alloc_num = alloc_num;
     MPI_File_seek(fp, (MPI_Offset)rank*alloc_num*sizeof(int), MPI_SEEK_SET);
 		if(rank==size-1){			
-			alloc_num = N-rank*alloc_num;
+			alloc_num = N - alloc_num * rank;
 		}	
+    last_alloc_num = N - alloc_num * rank;
 		array = (int*)malloc(sizeof(int)*alloc_num);
 		start = MPI_Wtime();
 		MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
 		finish = MPI_Wtime();
 		printf("rank: %2d io time: %lf\n",rank,finish-start);	
   }
+  //MPI_File_close(fp);
   // sheu
 	// todo
 	// sort and communicate with other
   //--------------------------------------------------------------------------------
-  int tmp1,tmp2,sorted_temp; 
-	int *tmp_ptr;
-	int i,j,k,sorted=0;
-	int nei_alloc;
+  int tmp1,tmp2,sorted_temp;
+  int *num_ptr, *pos_ptr; 
+  int i,sorted=0;
+#ifdef DEBUG
+  if(rank==ROOT){
+    root_ptr = (int*)calloc(0,sizeof(int)*N);	
+    num_ptr = (int*)malloc(sizeof(int)*size);
+    pos_ptr = (int*)malloc(sizeof(int)*size);
+    for(i=0;i<size;i++){
+      num_ptr[i] = former_alloc_num;
+      pos_ptr[i] = i * former_alloc_num;
+    }
+    num_ptr[size-1] = last_alloc_num;
+  }
+#endif
 	while(!sorted){
     sorted=1;
     for(i=0;i+1<alloc_num;i+=2){
@@ -181,9 +198,11 @@ int main (int argc, char *argv[]) {
         sorted = 0;
       }
     }
-    printf("rank: %2d %d %d\n", rank, array[0], array[1]);	
+    //printf("rank: %2d %d %d\n", rank, array[0], array[1]);	
     for(i=1;i<alloc_num;i+=2){
-      if(i=alloc_num-1){
+      if(i==alloc_num-1){
+        // has even elements, which is guaranteed but last process
+        
         // send to rank+1, last node do nothing
         if(rank!=size-1){
           MPI_Send(&array[i],1,MPI_INT,rank+1,0,MPI_COMM_WORLD);
@@ -209,423 +228,46 @@ int main (int argc, char *argv[]) {
           MPI_Recv(&tmp2,1,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
           array[i] = tmp2;
         }
-        /*if(array[i]>tmp2){
+        if(array[i]>tmp2&&rank!=size-1){
           swap(&array[i],&tmp2);
           sorted = 0;
+        }
+        /*if(rank==size-1){
+          puts("NOOOO");
         }*/
-        /*
-        // send to rank-1, last node do nothing
-        if(rank!=size-1){
-          MPI_Send(&array[i],1,MPI_INT,rank+1,0,MPI_COMM_WORLD);
-          //MPI_Recv(&tmp1,1,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-        }
-        // receive from rank+1, root node do nothing
-        if(rank!=ROOT){
-          MPI_Recv(&tmp1,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-        }
-        if(array[i]>tmp1){
-          swap(&array[i],&array[i+1]);
-          sorted = 0;
-        }
-        // receive part, root node do nothing
-        if(rank!=ROOT){
-          MPI_Recv(&tmp1,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-          MPI_Send(&array[i],1,MPI_INT,rank-1,0,MPI_COMM_WORLD);
-        }
-        */
+        continue;
       }
       else if(array[i]>array[i+1]){
         swap(&array[i],&array[i+1]);
-        sorted = 0;
+        sorted = 0;      
+      }
+      if(rank==size-1&&alloc_num%2&&i==alloc_num-2){
+        // has odd elements, only in last process
+        MPI_Recv(&tmp1,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);        
+        if(array[0]<tmp1){
+          swap(&array[0],&tmp1);
+          sorted = 0;
+        }
+        MPI_Send(&tmp1,1,MPI_INT,rank-1,0,MPI_COMM_WORLD);
       }
     }
-    printf("rank: %2d %d %d\n", rank, array[0], array[1]);
+    //printf("rank: %2d %d %d\n", rank, array[0], array[1]);	
     MPI_Allreduce(&sorted,&sorted_temp,1,MPI_INT,MPI_LAND,MPI_COMM_WORLD);
     sorted = sorted_temp;
   }
-
+#ifdef DEBUG
 	MPI_Barrier(MPI_COMM_WORLD);
-	printall(array,alloc_num);
-	if(rank==ROOT){
-		tmp_ptr = (int*)calloc(0,sizeof(int)*N);
+	//printall(array,alloc_num);
+	MPI_Gatherv(array, alloc_num, MPI_INT, root_ptr, num_ptr, pos_ptr, MPI_INT, ROOT, MPI_COMM_WORLD);
+  if(rank==ROOT){
+		printall(root_ptr, N);
 	}
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Gather(array,alloc_num,MPI_INT, tmp_ptr, alloc_num, MPI_INT, ROOT, MPI_COMM_WORLD);
-	if(rank==ROOT){
-		printall(tmp_ptr, N);
-		free(tmp_ptr);
-	}
-	MPI_Finalize();
-	exit(0);
-  
-
-	/*if(rank>=N){
-		// sheu need to change!
-		MPI_Finalize();
-		exit(0);
-	}
-	else{
-		if(N < size){
-			size -= size - N;
-		}
-		alloc_num = N / size;
-		if(N%size==0){
-			//todo
-			if(alloc_num==1){
-				// all go to  last process
-				if(rank!=size-1){
-					MPI_Finalize();
-					exit(0);
-				}
-				else{
-					// in this situation, last node handle all input using singleoesort
-					alloc_num = N;
-					MPI_File_seek(fp,(MPI_Offset)0, MPI_SEEK_SET);
-					array = (int*)malloc(sizeof(int)*alloc_num);
-					start = MPI_Wtime();
-					MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-					finish = MPI_Wtime();
-					printf("rank %2d io time: %lf\n", rank, finish - start);
-					singleOESort(array, alloc_num);
-					printall(array, alloc_num);
-					MPI_Finalize();
-					exit(0);
-			}
-			MPI_File_seek(fp, (MPI_Offset)rank*alloc_num, MPI_SEEK_SET);
-			array = (int*)malloc(sizeof(int)*alloc_num);
-			start = MPI_Wtime();
-			MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-			finish = MPI_Wtime();
-			printf("rank: %2d io time: %lf\n",rank,finish-start);
-		}
-		else{
-			// last process need to deal with more inputs
-			if(rank==size-1){
-				if(alloc_num%2){
-					alloc_num--;
-					MPI_File_seek(fp, (MPI_Offset)rank*alloc_num*sizeof(int), MPI_SEEK_SET);
-					alloc_num = N-rank*alloc_num+size-1;
-					// in some situation, last process need to deal with all process
-				}
-				else {
-					MPI_File_seek(fp, (MPI_Offset)rank*alloc_num*sizeof(int), MPI_SEEK_SET);
-					alloc_num = N-rank*alloc_num;
-				}
-				array = (int*)malloc(sizeof(int)*alloc_num);
-				start = MPI_Wtime();
-				MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-				finish = MPI_Wtime();
-				printf("rank: %2d io time: %lf\n",rank,finish-start);
-
-			}
-			else{
-				if(alloc_num%2)
-					alloc_num--;
-				MPI_File_seek(fp, (MPI_Offset)rank*alloc_num*sizeof(int), MPI_SEEK_SET);
-				array = (int*)malloc(sizeof(int)*alloc_num);
-				start = MPI_Wtime();
-				MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-				finish = MPI_Wtime();
-				printf("rank: %2d io time: %lf\n",rank,finish-start);
-
-			}*/
-			/*alloc_num+=1;
-			if(rank*alloc_num>=N){
-				printf("rank: %2d others take my job, exit\n",rank);
-				MPI_Finalize();
-				exit(0);
-			}
-			MPI_File_seek(fp, (MPI_Offset)rank*alloc_num*sizeof(int), MPI_SEEK_SET);
-			array = (int*)malloc(sizeof(int)*alloc_num);
-			if(rank*alloc_num+alloc_num>N){
-				alloc_num = N-rank*alloc_num;
-				start = MPI_Wtime();
-				MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-				finish = MPI_Wtime();
-				printf("rank: %2d io time: %lf\n",rank,finish-start);
-			}
-			else{
-				start = MPI_Wtime();
-				MPI_File_read(fp, array, alloc_num, MPI_INT, &status);
-				finish = MPI_Wtime();
-				printf("rank: %2d io time: %lf\n",rank,finish-start);
-			}*/
-			//test
-			/*
-			insertionsort(array,alloc_num);
-			printall(array,alloc_num);
-			MPI_Finalize();
-			exit(0);
-			*/
-	//	}
-	
-	// sheu
-	// todo
-	// sort and communicate with other
-    //--------------------------------------------------------------------------------
-/*	int time=0,sorted=0,temp;
-	int *temp_array = malloc(sizeof(int)*(alloc_num+size));
-	int *sorted_array = malloc(sizeof(int)*alloc_num);
-	int *tmp_ptr;
-	int i,j,k;
-	int nei_alloc;
-	while(!sorted){
-		sorted = 1;
-		printall(array,alloc_num);
-		if(time==0){
-			// from odd rank send to even rank
-			if(rank%2){
-				// odd rank
-				if(rank==size-1){
-					// do nothing
-				}
-				else{
-					MPI_Send(&alloc_num,1,MPI_INT,rank+1,0,MPI_COMM_WORLD);
-					MPI_Send(array,alloc_num,MPI_INT,rank+1,0,MPI_COMM_WORLD);
-					MPI_Recv(&nei_alloc,1,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Recv(temp_array,nei_alloc,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					
-				}
-			}
-			else{
-				if(rank==ROOT){
-					// do nothing
-				}
-				else{
-					MPI_Recv(&nei_alloc,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Recv(temp_array,nei_alloc,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Send(&alloc_num,1,MPI_INT,rank-1,0,MPI_COMM_WORLD);
-					MPI_Send(array,alloc_num,MPI_INT,rank-1,0,MPI_COMM_WORLD);
-				}
-			}
-			// merge
-			// odd has smaller part
-			if(rank%2){
-				if(rank!=size-1){
-					for(i=0,j=0,k=0;i<alloc_num;i++){
-						if(j==nei_alloc){
-							sorted_array[i]=array[k];
-							k++;
-							continue;
-						}
-						else if(k==alloc_num){
-							sorted_array[i]=temp_array[j];
-							j++;
-							continue;
-						}
-						if(temp_array[j]<array[k]){
-							sorted_array[i]=temp_array[j];
-							j++;
-						}
-						else{
-							sorted_array[i]=array[k];
-							k++;
-						}
-					}
-				}
-			}
-			else{
-			// even has larger part
-				if(rank!=ROOT){
-					for(i=alloc_num-1,j=nei_alloc-1,k=alloc_num-1;i>=0;i--){
-						if(j==-1){
-							sorted_array[i]=array[k];
-							k--;
-							continue;
-						}
-						else if(k==-1){
-							sorted_array[i]=temp_array[j];
-							j--;
-							continue;
-						}
-						if(temp_array[j]>array[k]){
-							sorted_array[i]=temp_array[j];
-							j--;
-						}
-						else{
-							sorted_array[i]=array[k];
-							k--;
-						}
-					}
-				}
-			}
-			if(rank!=ROOT&&(rank!=size-1||rank%2!=1)){
-				tmp_ptr=array;
-				array=sorted_array;
-				sorted_array=tmp_ptr;
-			}
-			if(0!=memcmp((const void*)array,(const void*)sorted_array,(size_t)sizeof(int)*alloc_num))
-				sorted=0;
-			time=1;
-		}
-		if(time==1){
-			// from even rank send to odd rank
-			if(!(rank%2)){
-				// even rank
-				if(rank==size-1){
-					// do nothing
-				}
-				else{
-					MPI_Send(&alloc_num,1,MPI_INT,rank+1,0,MPI_COMM_WORLD);
-					MPI_Send(array,alloc_num,MPI_INT,rank+1,0,MPI_COMM_WORLD);
-					MPI_Recv(&nei_alloc,1,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Recv(temp_array,nei_alloc,MPI_INT,rank+1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					
-				}
-			}
-			else{
-				{
-					MPI_Recv(&nei_alloc,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Recv(temp_array,nei_alloc,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-					MPI_Send(&alloc_num,1,MPI_INT,rank-1,0,MPI_COMM_WORLD);
-					MPI_Send(array,alloc_num,MPI_INT,rank-1,0,MPI_COMM_WORLD);
-				}
-			}
-			// merge
-//			printf("rank: %2d array",rank)
-			// even has smaller part
-			if(!(rank%2)){
-				if(rank!=size-1){
-					for(i=0,j=0,k=0;i<alloc_num;i++){
-						if(j==nei_alloc){
-							sorted_array[i]=array[k];
-							k++;
-							continue;
-						}
-						else if(k==alloc_num){
-							sorted_array[i]=temp_array[j];
-							j++;
-							continue;
-						}
-						if(temp_array[j]<array[k]){
-							sorted_array[i]=temp_array[j];
-							j++;
-						}
-						else{
-							sorted_array[i]=array[k];
-							k++;
-						}
-					}
-				}
-			}
-			else{
-			// odd has larger part
-				for(i=alloc_num-1,j=nei_alloc-1,k=alloc_num-1;i>=0;i--){
-					if(j==-1){
-						sorted_array[i]=array[k];
-						k--;
-						continue;
-					}
-					else if(k==-1){
-						sorted_array[i]=temp_array[j];
-						j--;
-						continue;
-					}
-					if(temp_array[j]>array[k]){
-						sorted_array[i]=temp_array[j];
-						j--;
-					}
-					else{
-						sorted_array[i]=array[k];
-						k--;
-					}
-				}
-			}
-			if(rank!=size-1||rank%2!=1){
-				tmp_ptr=array;
-				array=sorted_array;
-				sorted_array=tmp_ptr;
-			}
-			if(0!=memcmp((const void*)array,(const void*)sorted_array,(size_t)sizeof(int)*alloc_num))
-				sorted=0;
-
-			time=0;
-		}
-		MPI_Allreduce(&sorted,&temp,1,MPI_INT,MPI_LAND,MPI_COMM_WORLD);
-		sorted = temp;
-	}
-	printf("rank: %2d finish %d\n", rank, array[0]);
-	
-	//
-	free(temp_array);
-	if(rank==ROOT)
-		temp_array = (int*)calloc(0,sizeof(int)*N);
-	
-	MPI_Gather(array,alloc_num,MPI_INT, temp_array, N, MPI_INT, ROOT, MPI_COMM_WORLD);
-	if(rank==ROOT)
-		printall(temp_array, N);
-	MPI_Finalize();
-
-	exit(0);
-*/
-
-
-
-
-
-
-
-
-
-
-/*	if (rank == ROOT) {
-		struct stat st;
-		int fd = 0;
-		fd = open(argv[2],O_RDONLY);
-		fstat(fd, &st);
-		if(fd != -1){
-			puts("N is bigger than testcase in input file, read to the end");
-		N = st.sts_size/sizeof(int);
-		}
-		else{
-			puts("ferror QQ");
-			MPI_Barrier(MPI_COMM_WORLD);
-			MPI_Finalize();
-		}
-		close(fd);
-	}
-	MPI_Barrier(MPI_COMM_WORLD);
-*/
-/*		root_arr = malloc(N * sizeof(int));
-		FILE *f = fopen(inName, "rb"); // sheu may need change
-		fread(root_arr, sizeof(int), N, f);
-		fclose(f);
-	}
-*/
-
-	// Part 2: Scatter
-	/* Note: You should deal with cases where (N % size != 0) in Homework 1 */
-/*	int num_per_node = N / size;
-	int *local_arr = malloc (num_per_node * sizeof(int));
-	// Ref:
-	// MPI_Scatter (send_buf, send_count, send_type, recv_buf, recv_count, recv_type, root, comm)
-	MPI_Scatter    (root_arr, num_per_node, MPI_INT, local_arr, num_per_node, MPI_INT, ROOT, MPI_COMM_WORLD);
-
-	if (rank == ROOT) {
-		free (root_arr);
-	}
-
-	// Part 3: Calculate the sum of local_arr
-	int sum = 0;
-	//int i;
-	for (i = 0; i < num_per_node; i++) {
-		sum += local_arr[i];
-	}
-
-	free (local_arr);
-
-	// Part 4: Accumulate the result to root process
-	int accumulated;
-	// Ref:
-	// MPI_Reduce (send_buf, recv_buf, count, data_type, op, root, comm)
-	MPI_Reduce    (&sum, &accumulated, 1, MPI_INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
-
-	if (rank == ROOT) {
-		printf("SUM: %d\n", accumulated);
-	}
-*/
+#endif
+  MPI_File fh;
+  MPI_File_open(MPI_COMM_WORLD, outName, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
+  MPI_Offset my_offset = rank*former_alloc_num*sizeof(int);
+  MPI_File_write_at(fh, my_offset, array, alloc_num, MPI_INT, &status);
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Finalize();
-
 	return 0;
 }
